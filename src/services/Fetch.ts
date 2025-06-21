980f0068465e1b030816984b7554ec3acfb59d6d
Zephyr-Jeon <rltjd1258@gmail.com>
2025-06-20 17:24:24 -0700

    updated fetch methods

--- src/services/Fetch.ts
+++ src/services/Fetch.ts
@@ -3,82 +3,91 @@ import { StatusCodes } from 'http-status-codes';
 export class Fetch {
   static baseInit: RequestInit = {
     credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-    },
   };
 
-  static async get(input: RequestInfo) {
+  static baseHeaders: HeadersInit = {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  };
+
+  static async get(input: RequestInfo, init: RequestInit = {}) {
+    const { headers, body, ...rest } = init;
+
     return this.responseHandler(() =>
       fetch(input, {
         ...this.baseInit,
         method: 'GET',
+        headers: {
+          ...this.baseHeaders,
+          ...headers,
+        },
+        body: body ? JSON.stringify(body) : undefined,
+        ...rest,
       })
     );
   }
 
-  static async post(input: RequestInfo, body: {}) {
+  static async post(input: RequestInfo, init: RequestInit = {}) {
+    const { headers, body, ...rest } = init;
+
     return this.responseHandler(() =>
       fetch(input, {
         ...this.baseInit,
         method: 'POST',
-        body: JSON.stringify(body),
+        headers: {
+          ...this.baseHeaders,
+          ...headers,
+        },
+        body: body ? JSON.stringify(body) : undefined,
+        ...rest,
       })
     );
   }
 
-  static async put(input: RequestInfo, body: {}) {
+  static async put(input: RequestInfo, init: RequestInit = {}) {
+    const { headers, body, ...rest } = init;
+
     return this.responseHandler(() =>
       fetch(input, {
         ...this.baseInit,
         method: 'PUT',
-        body: JSON.stringify(body),
+        headers: {
+          ...this.baseHeaders,
+          ...headers,
+        },
+        body: body ? JSON.stringify(body) : undefined,
+        ...rest,
       })
     );
   }
 
-  static async delete(input: RequestInfo, body: {}) {
+  static async delete(input: RequestInfo, init: RequestInit = {}) {
+    const { headers, body, ...rest } = init;
+
     return this.responseHandler(() =>
       fetch(input, {
         ...this.baseInit,
         method: 'DELETE',
-        body: JSON.stringify(body),
+        headers: {
+          ...this.baseHeaders,
+          ...headers,
+        },
+        body: body ? JSON.stringify(body) : undefined,
+        ...rest,
       })
     );
   }
 
   private static async responseHandler(request: () => Promise<Response>) {
-    try {
-      let body: any;
-      const response = await request();
+    let body: any;
+    const response = await request();
 
-      if (response.headers.get('Content-Type')?.includes?.('json')) {
-        body = await response.json();
-      } else {
-        body = await response.text();
-      }
-
-      if (response.ok) {
-        return body;
-      } else {
-        // TODO: error handling
-        switch (response.status) {
-          case StatusCodes.BAD_REQUEST:
-          case StatusCodes.UNAUTHORIZED:
-          case StatusCodes.NOT_FOUND:
-          case StatusCodes.CONFLICT:
-          case StatusCodes.TOO_MANY_REQUESTS:
-          case StatusCodes.INTERNAL_SERVER_ERROR:
-          case StatusCodes.BAD_GATEWAY:
-          case StatusCodes.GATEWAY_TIMEOUT:
-          default:
-            console.log('Fetch Error', response, body);
-            return { response, body };
-        }
-      }
-    } catch {
-      console.log('Unknown Fetch Error');
+    if (response.headers.get('Content-Type')?.includes?.('json')) {
+      body = await response.json();
+    } else {
+      body = await response.text();
     }
+
+    return { response, body };
   }
 }
